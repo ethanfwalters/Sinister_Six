56b4d34ee412c37f104d375610a7d05b2a2ef0d3
daba1485 <daba1485@colorado.edu>
2021-02-16 16:02:02 -0700

    Changing some stuff with the format of the components

--- spotitry/src/App.js
+++ spotitry/src/App.js
@@ -1,53 +1,46 @@
 import logo from './images/logo.svg';
 import styles from  './App.module.css';
-import React, { Component } from "react";
-// import styles from './real.module.css'
-
-// import hash from "./hash";
+import React, { Component, useEffect, useState } from "react";
 export const authEndpoint = 'https://accounts.spotify.com/authorize?';
 export const signUp = 'https://www.spotify.com/signup/'
-// Replace with your app's client ID, redirect URI and desired scopes
 
 const spotifyLogo = 'https://logos-world.net/wp-content/uploads/2020/09/Spotify-Symbol.png'
-const clientId = "4604d772bd3e4fe69399830809371aa4";
-const redirectUri ="http://localhost:3000/home"
-const scopes = [
-  "user-read-currently-playing",
-  "user-read-playback-state",
-];
-// Get the hash of the url
-const hash = window.location.hash
-  .substring(1)
-  .split("&")
-  .reduce(function(initial, item) {
-    if (item) {
-      var parts = item.split("=");
-      initial[parts[0]] = decodeURIComponent(parts[1]);
-    }
-    return initial;
-  }, {});
-window.location.hash = "";
+  const clientId = "4604d772bd3e4fe69399830809371aa4";
+  const redirectUri ="http://localhost:3000/home"
+  const scopes = [
+    "user-read-currently-playing",
+    "user-read-playback-state",
+  ];
+  // Get the hash of the url
+  const hash = window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce(function(initial, item) {
+      if (item) {
+        var parts = item.split("=");
+        initial[parts[0]] = decodeURIComponent(parts[1]);
+      }
+      return initial;
+    }, {});
+  window.location.hash = "";
 
-console.log(hash)
-
-class App extends Component {
-  componentDidMount() {
-    // Set token
+const App = () => {
+  const [accessToken, setAccessToken] = useState()
+  useEffect(() => {
     let _token = hash.access_token;
     if (_token) {
       // Set token
-      this.setState({
+      setAccessToken({
         token: _token
       });
     }
-  }
-render() {
+  },[hash.access_token])
   return (
     <div className={styles.App}>
       <header className={styles.AppName}>SpotiTRY</header>
       <header className={styles.AppHeader}>
       <img src={spotifyLogo} className={styles.AppLogo} alt="Spotify Logo" />
-      {!this?.state?.token && (
+      {!accessToken && (
         <a
           className={styles.AppLink}
           href={`${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&response_type=token&show_dialog=true`}
@@ -56,7 +49,7 @@ render() {
         </a>
         
       )}
-      {!this?.state?.token && (
+      {!accessToken && (
         <a
           className={styles.AppLink}
           href={`${signUp}redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}
@@ -67,12 +60,7 @@ render() {
       </header>
     </div>
   );
-  }
 }
 
 export default App;
 
-
-// const App= ( => {
-
-// })
